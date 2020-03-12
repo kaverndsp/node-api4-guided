13e2b83e5b392410ad3cd45c10ad390bc0767d4f
kaverndsp <kaverndsp@users.noreply.github.com>
2020-03-12 11:37:58 -0500

    deployment changes

--- api/api-router.js
+++ api/api-router.js
@@ -7,7 +7,9 @@ const router = express.Router();
 router.use(express.json());
 
 router.get('/', (req, res) => {
-  res.status(200).json({ api: 'up' });
+  const enviroment = process.env;
+  const port = process.env.PORT || 4000;
+  res.status(200).json({ api: 'up', port, enviroment });
 });
 
 router.get('/shouts', (req, res, next) => {
